3b96107d0ef8c2f7fcf85752979882f320a24c1f
Carlos Parra <carlos.parar.sbr@gmail.com>
2018-06-04 23:16:21 -0400

    Adding initial files

---
+++ app/src/main/java/com/example/carlosparra/exercise01/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.carlosparra.exercise01.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.carlosparra.exercise01.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
